4ce55a255426b7439dc8f2799d324f3726839377
GauthierDenglos <gauthier.denglos@epita.fr>
2021-09-30 10:19:49 +0200

    [ADD][MAIN] first RESTfull API

--- main.go
+++ main.go
@@ -1,12 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
-	"rsc.io/quote"
+	"log"
+	"net/http"
 )
 
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title", Desc: "Test Desc", Content: "Hello World"},
+	}
+
+	fmt.Println("Endpoint Hit : All articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Homepage Endpoint Hit")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8001", nil))
+}
+
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println(quote.Go())
+	handleRequests()
 }
